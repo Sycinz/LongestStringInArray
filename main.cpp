640cc9764c6de197932e022d1cddf165a9f15857
Sycinz <60658488+Sycinz@users.noreply.github.com>
2022-10-20 21:34:59 +0200

    Uploading main.cpp file

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string list[10];
+    int arr[10], count[10], j, n{10};
+
+    for (int i = 0; i < 10; i++) {
+        std::getline(std::cin, list[i]);
+    }
+
+    for(int i = 0; i < 10; i++) {
+        std::cout << list[i] << " "; 
+    }
+
+    for(j = 1; j < n; ++j) {
+        if(arr[0] < arr[j]) {
+            arr[0] = arr[j];
+        }
+    }
+
+    for(int i = 0; i < 10; i++) {
+        if(list[i].length() == arr[0]) {
+            std::cout << "Longest word in array: " << list[i] << std::endl;
+        }
+    }
+
+    return 0;
+}
